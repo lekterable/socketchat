2bac651be751541b05f9c82a47aa3d5a1f6e28e5
Kornel Dubieniecki <lekterable@gmail.com>
2017-10-01 04:18:48 +0200

    Logowanie, lista zalogowanych

--- app.js
+++ app.js
@@ -5,38 +5,59 @@ const express = require('express');
 const app = express()
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
+
 //Folder Publiczny
 app.use(express.static(path.join(__dirname, 'public')))
-//Strona główna
-app.get('/', (req, res)=>{
-  res.sendFile('./index.html', {root:__dirname})
-})
 //Połączenie z bazą
 mongodb.connect(config.database, (err, db)=>{
   if(err)
     throw err
   console.log('Połączono z bazą')
   let chat = db.collection('chats')
+  let users = []
   //Połączenie z serwerem Socket.IO
   io.on('connection', (socket)=>{
+    socket.on('login', (data, cb)=>{
+      if (users.indexOf(data.username)!=-1) {
+        if(!socket.logged)
+          socket.emit('info', {message:'Nazwa użytkownika jest już zajęta!', type:'alert alert-danger'})
+        return cb(false)
+      }
+      socket.username = data.username
+      socket.logged = true
+      users.push(data.username)
+      io.emit('users', users)
+      socket.emit('info', {message:'Zalogowano!', type:'alert alert-success'})
+      return cb(true)
+    })
+    socket.emit('users', users)
     socket.emit('cleared')
     chat.find().limit(100).sort({_id:1}).toArray((err, res)=>{
       if(err)
         throw err
       socket.emit('chat message', res)
     })
-    socket.on('new message', (message)=>{
-      if(message[0].name=='' || message[0].data==''){
-        socket.emit('info', {message:'Uzupełnij imię i wiadomość', type:'alert alert-danger'})
+    socket.on('new message', (message, cb)=>{
+      if(message[0].data==''){
+        socket.emit('info', {message:'Uzupełnij wiadomość!', type:'alert alert-danger'})
+        return cb(false)
+      }
+      else if (!socket.logged) {
+        socket.emit('info', {message:'Zaloguj się!', type:'alert alert-danger'})
+        return cb(false)
       }
       else {
         chat.insert({
-          name: message[0].name,
+          name: socket.username,
           data: message[0].data
         })
-        io.emit('chat message', message)
+        io.emit('chat message', [{
+          name: socket.username,
+          data: message[0].data
+        }])
         socket.emit('info', {message:'Wysłano wiadomość', type:'alert alert-success'})
         socket.broadcast.emit('info', {message:'Nowa wiadomość', type:'alert alert-info'})
+        return cb(true)
       }
     })
     socket.on('clear', ()=>{
@@ -44,6 +65,11 @@ mongodb.connect(config.database, (err, db)=>{
         io.emit('cleared')
         io.emit('info', {message: 'Czat wyczyszczony', type:'alert alert-info'})
       })
+    })
+    socket.on('disconnect', ()=>{
+      if(socket.logged){
+        users.splice(users.indexOf(socket.username), 1)
+        io.emit('users', users)}
     })
   })
 })
